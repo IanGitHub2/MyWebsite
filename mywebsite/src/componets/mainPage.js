450d8d0370d036e5031c6e602d63de31868d42a2
Ian Kincaid <ikbusinessacct@gmail.com>
2020-10-19 20:08:31 -0400

    init

---
+++ mywebsite/src/componets/mainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import { Link } from 'react-router-dom'
+
+export default class mainPage extends React.Componet {
+ componetDidMount(){
+     console.log('mainPage mounted')
+ }
+
+ render(){
+     return(
+         <div>
+             <div>Hi</div>
+         </div>
+     )
+ }
+}
