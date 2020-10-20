a25564cc421cf526581d6ae563692e9705dd151e
Ian Kincaid <ikbusinessacct@gmail.com>
2020-10-20 04:07:08 -0400

    working with projects area fliped colors and changed links the a tags and now adding images for projects

--- mywebsite/src/componets/myProjects.js
+++ mywebsite/src/componets/myProjects.js
@@ -1,39 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 import './myProjects.css'
 
 export default class myProjects extends React.Component {
     render(){
         return(
             <div>
+              <div id='projectsheader'>My Projects</div>
               <div id='projectsContainer'>
-                <div id='projectsheader'>My Projects</div>
                 <div id='eachproject'>
                     <div className='projecttitle'>Connect 4</div>
-                    <Link to={'https://infallible-mcnulty-f386b4.netlify.app'}>
-                    <div>Link to website</div>
-                    </Link>
+                    <a href='https://infallible-mcnulty-f386b4.netlify.app' target="_blank" rel='noopener noreferrer'>Link to website</a>
                     <div> Connect 4 give it a try.</div>
+                    <img alt='Connect 4' src='./connect4.png'/>
                 </div>
                 <div id='eachproject'>
                     <div className='projecttitle'>Movie Site</div>
-                    <Link to={'https://brave-nobel-691c58.netlify.app'}>
-                    <div>Link to website</div>
-                    </Link>
+                    <a href='https://brave-nobel-691c58.netlify.app' target="_blank" rel='noopener noreferrer'>Link to website</a>
                     <div> A parody of say netflix or hulu.</div>
                 </div>
                 <div id='eachproject'>
                     <div className='projecttitle'>Rush</div>
-                    <Link to={'https://rush-project-3.herokuapp.com/'}>
-                    <div>Link to website</div>
-                    </Link>
+                    <a href='https://rush-project-3.herokuapp.com/' target="_blank" rel='noopener noreferrer'>Link to website</a>
                     <div> Rush is like a social media website.</div>
                 </div>
                 <div id='eachproject'>
                     <div className='projecttitle'>C.P.S</div>
-                    <Link to={'https://infinite-badlands-56464.herokuapp.com/company/'}>
-                    <div>Link to website</div>
-                    </Link>
+                    <a href='https://infinite-badlands-56464.herokuapp.com/company/' target="_blank" rel='noopener noreferrer'>Link to website</a>
                     <div>C.P.S stands for Company / Products / Sellers</div>
                 </div>
               </div>
