280916e6eba54900710b9ebe3e26a76b4865a4ab
Ian Kincaid <ikbusinessacct@gmail.com>
2020-10-20 01:24:04 -0400

    Up and running layout in progress working on back ground and color scheme

--- mywebsite/src/componets/myProjects.js
+++ mywebsite/src/componets/myProjects.js
@@ -3,7 +3,19 @@ import React from 'react';
 export default class myProjects extends React.Component {
     render(){
         return(
-            <div>sides</div>
+            <div>
+              <div>
+                <div>My Projects</div>
+                <div>
+                    <h1>Connect 4</h1>
+                    <div>Project</div>
+                </div>
+                <div>
+                    <h1>Other Stuff</h1>
+                    <div>Project</div>
+                </div>
+              </div>
+            </div>
         )
     }
 }
