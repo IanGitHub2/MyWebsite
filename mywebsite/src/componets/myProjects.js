fc283c42e1a9c879e497cd5a79d6fd03ee61b873
Ian Kincaid <ikbusinessacct@gmail.com>
2020-10-21 03:49:14 -0400

    animations added images folder added

--- mywebsite/src/componets/myProjects.js
+++ mywebsite/src/componets/myProjects.js
@@ -10,23 +10,23 @@ export default class myProjects extends React.Component {
                 <div id='eachproject'>
                     <div className='projecttitle'>Connect 4</div>
                     <a href='https://infallible-mcnulty-f386b4.netlify.app' target="_blank" rel='noopener noreferrer'>Link to website</a>
-                    <div> Connect 4 give it a try.</div>
-                    <img className='projectimages' alt='Connect 4' src='./connect4.png'/>
+                    <div className='projectdef'> Connect 4 give it a try.</div>
+                    <img className='projectimages' alt='Connect 4' src='./images/connectgame.png'/>
                 </div>
                 <div id='eachproject'>
                     <div className='projecttitle'>Movie Site</div>
                     <a href='https://brave-nobel-691c58.netlify.app' target="_blank" rel='noopener noreferrer'>Link to website</a>
-                    <div> A parody of say netflix or hulu.</div>
+                    <div className='projectdef'> A parody of say netflix or hulu.</div>
                 </div>
                 <div id='eachproject'>
                     <div className='projecttitle'>Rush</div>
                     <a href='https://rush-project-3.herokuapp.com/' target="_blank" rel='noopener noreferrer'>Link to website</a>
-                    <div> Rush is like a social media website.</div>
+                    <div className='projectdef'> Rush is like a social media website.</div>
                 </div>
                 <div id='eachproject'>
                     <div className='projecttitle'>C.P.S</div>
                     <a href='https://infinite-badlands-56464.herokuapp.com/company/' target="_blank" rel='noopener noreferrer'>Link to website</a>
-                    <div>C.P.S stands for Company / Products / Sellers</div>
+                    <div className='projectdef'>C.P.S stands for Company / Products / Sellers</div>
                 </div>
               </div>
             </div>
