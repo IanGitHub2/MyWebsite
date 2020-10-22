412f6baa1221c1f9e2075ef65fc6dc494a581757
Ian Kincaid <ikbusinessacct@gmail.com>
2020-10-22 00:35:50 -0400

    images added titles moved and animated

--- mywebsite/src/componets/mainPage.js
+++ mywebsite/src/componets/mainPage.js
@@ -19,8 +19,11 @@ export default class mainPage extends React.Component {
                     <p id='jobtext'>Software Engineer</p>
                 </div>
              </div>
+             <div>
+                <div id='aboutheader'>About Me</div>
+                <div id='skillsheader'>Skills</div>
+             </div>
              <div id='aboutmecontainer'>
-                 <div id='aboutheader'>About Me</div>
                  <p id='aboutsum'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
                  <div id='logocontainer'>
--- mywebsite/src/componets/myProjects.js
+++ mywebsite/src/componets/myProjects.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './myProjects.css'
+import Connect4 from '../images/connectgame.png'
+import Homemovies from '../images/homemovies.png'
+import Cps from '../images/cps.png'
+import Rush from '../images/rush.png'
 
 export default class myProjects extends React.Component {
     render(){
@@ -11,22 +15,25 @@ export default class myProjects extends React.Component {
                     <div className='projecttitle'>Connect 4</div>
                     <a href='https://infallible-mcnulty-f386b4.netlify.app' target="_blank" rel='noopener noreferrer'>Link to website</a>
                     <div className='projectdef'> Connect 4 give it a try.</div>
-                    <img className='projectimages' alt='Connect 4' src='./images/connectgame.png'/>
+                    <img className='projectimages' alt='Connect 4' src={Connect4}/>
                 </div>
                 <div id='eachproject'>
                     <div className='projecttitle'>Movie Site</div>
                     <a href='https://brave-nobel-691c58.netlify.app' target="_blank" rel='noopener noreferrer'>Link to website</a>
                     <div className='projectdef'> A parody of say netflix or hulu.</div>
+                    <img className='projectimages' alt='Connect 4' src={Homemovies}/>
                 </div>
                 <div id='eachproject'>
                     <div className='projecttitle'>Rush</div>
                     <a href='https://rush-project-3.herokuapp.com/' target="_blank" rel='noopener noreferrer'>Link to website</a>
                     <div className='projectdef'> Rush is like a social media website.</div>
+                    <img className='projectimages' alt='Connect 4' src={Rush}/>
                 </div>
                 <div id='eachproject'>
                     <div className='projecttitle'>C.P.S</div>
                     <a href='https://infinite-badlands-56464.herokuapp.com/company/' target="_blank" rel='noopener noreferrer'>Link to website</a>
                     <div className='projectdef'>C.P.S stands for Company / Products / Sellers</div>
+                    <img className='projectimages' alt='Connect 4' src={Cps}/>
                 </div>
               </div>
             </div>
