0911622a7775052fb40f0f63971a2a87bc4fb7b4
Ian Kincaid <ikbusinessacct@gmail.com>
2020-10-25 01:23:24 -0400

    ok so i added new file to clean up code i fixed layout so it will be easier to optimize for other devices and still not done adding social media section

---
+++ mywebsite/src/componets/socialMedia.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './socialMedia.css'
+
+export default class socialMedia extends React.Component{
+    render(){
+        return(
+            <div id='Socialmediahousing'>
+              <div>
+                  <div>Social Media</div>
+              </div>
+              <div>
+                  <div>Links</div>
+              </div>
+          </div>
+        )
+    }
+}
